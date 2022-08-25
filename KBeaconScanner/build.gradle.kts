e137ac4fb7da3b37887b27712c49220312bacafd
Anna Labellarte <anna.labellarte.al@gmail.com>
2022-08-25 10:45:06 +0200

    add artifactory

--- KBeaconScanner/build.gradle.kts
+++ KBeaconScanner/build.gradle.kts
@@ -1,16 +1,23 @@
 plugins {
     kotlin("multiplatform")
     kotlin("native.cocoapods")
+    id("maven-publish")
     id("com.android.library")
+    id("com.jfrog.artifactory") version "4.29.0"
+    id("org.jetbrains.dokka") version "1.7.10"
 }
-
-version = "1.0"
+group = "com.annalabellarte.testScan"
+version = "1.0.1"
 
 kotlin {
-    android()
+
     iosX64()
     iosArm64()
     iosSimulatorArm64()
+
+    android {
+        publishLibraryVariants("release", "debug")
+    }
 
     cocoapods {
         summary = "Some description for the Shared Module"
@@ -28,6 +35,7 @@ kotlin {
             dependencies {
                 implementation("co.touchlab:kermit:1.1.3")
                 implementation("org.jetbrains.kotlinx:kotlinx-coroutines-core:1.6.4")
+
             }
         }
         val commonTest by getting {
@@ -69,5 +77,76 @@ android {
     defaultConfig {
         minSdk = 23
         targetSdk = 32
+    }
+}
+publishing{
+    publications {
+        register("mavenKotlin", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner"
+            version = "1.0.1"
+            artifact("$buildDir/libs/KBeaconScanner-kotlin-1.0.1-sources.jar")
+
+        }
+        register("mavenAndroidDebug", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner-android-debug"
+            version = "1.0.1"
+            artifact("$buildDir/libs/KBeaconScanner-android-debug-1.0.1-sources.jar")
+
+        }
+
+        register("mavenAndroid", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner-android"
+            version = "1.0.1"
+            artifact("$buildDir/libs/KBeaconScanner-android-1.0.1-sources.jar")
+
+        }
+
+        register("mavenIosArm64", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner-iosarm64"
+            version = "1.0.1"
+            artifact("$buildDir/libs/KBeaconScanner-iosarm64-1.0.1-sources.jar")
+
+        }
+        register("mavenIosSimulatorArm64", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner-iossimulatorarm64"
+            version = "1.0.1"
+            artifact("$buildDir/libs/KBeaconScanner-iossimulatorarm64-1.0.1-sources.jar")
+
+        }
+
+        register("mavenIosx86", MavenPublication::class) {
+            groupId = "com.annalabellarte.testScan"
+            artifactId = "KBeaconScanner-iosx64"
+            version = "1.0.1"
+
+            artifact("$buildDir/libs/KBeaconScanner-iosx64-1.0.1-sources.jar")
+
+        }
+
+
+    }
+}
+
+artifactory{
+    setContextUrl("https://annalabellarte.jfrog.io/artifactory")
+    publish{
+        setContextUrl("https://annalabellarte.jfrog.io/artifactory")
+        repository {
+            setRepoKey("playground")
+            setUsername("labe.anna97@gmail.com")
+            setPassword("15-rK9mTiGvX")
+            setMavenCompatible(true)
+        }
+
+        defaults {
+            publications("mavenKotlin", "mavenAndroid",
+                "mavenAndroidDebug", "mavenIosx86", "mavenIosSimulatorArm64", "mavenIosArm64")
+        }
+
     }
 }
